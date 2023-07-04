a8fcbdde89d3bcc6b0ab8dc123e2c95f05710563
benis20 <benj.caro@duocuc.cl>
2023-07-03 20:49:54 -0400

    subiendo script

---
+++ benjamin caro y bryan santibañez.py
@@ -0,0 +1,9 @@
+NAME= ""
+NAME2= ""
+
+NAME=str(input("INGRESE NOMBRE y APELLIDO "))
+NAME2=str(input("INGRESE NOMBRE y APELLIDO DEL SEGUNDO INTEGRANTE "))
+
+print("la lista de los integrantes es :")
+print(NAME)
+print(NAME2)
